ee594d40c024450899c71c54319d77aff5f89894
estherlozanoromero <estherlozanoromero@gmail.com>
2023-05-13 15:14:18 +0200

    primers inserts

---
+++ BaseDades/insertAnswer.sql
@@ -0,0 +1,6 @@
+insert into Answer(1, 'Madrid', 'Capitals');
+insert into Answer(2, 'Paris', 'Capitals');
+insert into Answer(3, 'Brussels', 'Capitals');
+insert into Answer(4, 'Amsterdam', 'Capitals');
+insert into Answer(5, 'Oslo', 'Capitals');
+
