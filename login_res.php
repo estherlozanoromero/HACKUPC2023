2326751348ac608863eb284c8998a7a41c14a186
vivesan121 <andreu.vives@estudiantat.upc.edu>
2023-05-13 05:34:17 -0700

    db i login

--- login_res.php
+++ login_res.php
@@ -1,18 +1,49 @@
 <?php
 session_start();
 if(isset($_SESSION['Auth']) && $_SESSION['Auth'] == "SI"){
-header('Location: ./Ranking.php');
+	header('Location: ./Ranking.php');
 }
+echo "pre require<br>";
+require_once('db_connection.php');
+echo "post require<br>";
+
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	echo "entrem post<br>";
     // Get the selected answer from the form
     $username = $_POST["username"];
     $seat = $_POST["seat"];
-	$_SESSION['Auth'] = "Si";
-	$_SESSION['username'] = $username;
-	$_SESSION['seat'] = $seat;
+    $sql = "SELECT seat, username FROM Player WHERE seat = '$seat'";
+	$result = $conn->query($sql);
+	echo "Primera consulta<br>";
+	
+	if ($result->num_rows > 0) {
+	echo "Hi ha files <br>";
+  	// output data of each row
+  	if ($result['username'] == $username){
+  		$_SESSION['Auth'] = "Si";
+		$_SESSION['username'] = $username;
+		$_SESSION['seat'] = $seat;
+  	}
+  	else{
+  		$_SESSION['Auth'] = "No";
+  		$_SESSION['error'] = "Wrong username";
+  	}
+	} 	
+	
+	else {
+  		$_SESSION['Auth'] = "Si";
+		$_SESSION['username'] = $username;
+		$_SESSION['seat'] = $seat;
+		$sql2 = "INSERT INTO Player (seat, username)VALUES ($seat, $username); ";
+		$result2 = $conn->query($sql2);
+		
+	}
+
 }
+mysqli_close($conn);
 header('Location: ./Ranking.php');
+exit();
 ?>
 
 
