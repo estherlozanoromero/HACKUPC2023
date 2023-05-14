67dea9c39b471d65a52b5f540dd8ebdcef33e761
estherlozanoromero <55650862+estherlozanoromero@users.noreply.github.com>
2023-05-14 06:05:01 +0200

    Update insertAnswer.sql

--- BaseDades/insertAnswer.sql
+++ BaseDades/insertAnswer.sql
@@ -3,4 +3,22 @@ insert into Answer values(2, 'Paris', 'Capitals');
 insert into Answer values(3, 'Brussels', 'Capitals');
 insert into Answer values(4, 'Amsterdam', 'Capitals');
 insert into Answer values(5, 'Oslo', 'Capitals');
+insert into Answer values(6, 'Berlin', 'Capitals');
+insert into Answer values(7, 'Washington D.C.', 'Capitals');
+
+insert into Answer values(8, 'Paella', 'Foods');
+insert into Answer values(9, 'Croissant', 'Foods');
+insert into Answer values(10, 'Waffles', 'Foods');
+insert into Answer values(11, 'Poffertjes', 'Foods');
+insert into Answer values(12, 'Farika', 'Foods');
+insert into Answer values(13, 'Bratwurst', 'Foods');
+insert into Answer values(14, 'Burger', 'Foods');
+
+insert into Answer values(15, 'La Sagrada Familia', 'Monouments');
+insert into Answer values(16, 'Eiffel Tower', 'Foods');
+insert into Answer values(17, 'Manneken Pis', 'Foods');
+insert into Answer values(18, 'Defence Line', 'Foods');
+insert into Answer values(19, 'Bergen House', 'Foods');
+insert into Answer values(20, 'Bratwurst', 'Foods');
+insert into Answer values(21, 'Liberty Statue', 'Foods');
 
